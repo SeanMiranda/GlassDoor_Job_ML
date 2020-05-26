9ec4d6c72b1f37f213d4aee7c41082e5aca61bf2
Sean Miranda <sean.miranda@gmail.com>
2020-05-26 17:39:38 +0530

    Data Cleaning update

--- data_cleaning.py
+++ data_cleaning.py
@@ -6,8 +6,12 @@ Created on Sat May 23 16:42:29 2020
 """
 
 import pandas as pd
+from datetime import date
+
 
 df = pd.read_csv('glassdoor_jobs.csv')
+
+#Salary
 
 df['hourly'] = df['Salary Estimate'].apply(lambda x: 1 if 'per hour' in x.lower() else 0)
 df['employer_provided'] = df['Salary Estimate'].apply(lambda x: 1 if 'employer provided salary' in x.lower() else 0)
@@ -22,3 +26,40 @@ df['min_salary'] = min_hr.apply(lambda x: int(x.split('-')[0]))
 df['max_salary'] = min_hr.apply(lambda x: int(x.split('-')[1]))
 
 df['avg_salary'] = (df.min_salary+df.max_salary)/2
+
+
+# Company name
+
+df['company_txt'] = df.apply(lambda x: x['Company Name'] if x['Rating']<0 else x['Company Name'][:-3], axis=1)
+
+# State Field
+
+df['job_state'] = df['Location'].apply(lambda x: x.split(',')[1])
+# print(df.job_state.value_counts())
+
+df['same_state'] = df.apply(lambda x: 1 if x.Location == x.Headquarters else 0, axis=1)
+
+# Age of the company
+df['age'] = df.Founded.apply(lambda x: x if x<1 else int(date.today().year)-x)
+
+# Parsing job description
+
+skill_dict = {'python_yn' : ['python'],
+              'R_yn' : ['r studio', 'r-studio'],
+              'spark' : ['spark'],
+              'aws' : ['aws'],
+              'excel' : ['excel'],
+              'tableau': ['tableau'],
+              'jmp': ['jmp'],
+              'power_bi': ['power bi'],
+              'sas' : ['sas']
+}
+
+for key, val in skill_dict.items():
+    df[key] = df['Job Description'].apply(lambda x: 1 if any(ele in x.lower() for ele in val) else 0)
+    print(df[key].value_counts())
+    
+print(df.columns)
+df_out = df.drop(['Unnamed: 0'], axis=1)
+
+df_out.to_csv('salary_data_cleaned.csv', index=False)
