31a974a62677681c33b5181520c1b6d853270460
Sean Miranda <sean.miranda@gmail.com>
2020-06-05 19:07:46 +0530

    Update Model_Building.py

--- Model_Building.py
+++ Model_Building.py
@@ -8,3 +8,16 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
+df = pd.read_csv('salary_data_cleaned.csv')
+print(df.columns)
+print(df.head())
+# Choose relevant columns
+
+# Get dummy data
+# Train test split
+# Multiple linear regression
+# Lasso Regression
+# Random Forest
+# Tune models GridsearchCV
+# Test ensable
+
